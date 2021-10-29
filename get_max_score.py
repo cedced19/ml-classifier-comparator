3089ba01473e44e972691fd3da196fb528adbb77
Cédric JUNG <cedced19@gmail.com>
2021-10-29 09:45:43 +0200

    Add function

---
+++ get_max_score.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array = np.load('results/brazil_dataset_french_clean.npy', allow_pickle=True)
+
+max_score = 0
+max_score_method = None
+for experiment in array:
+    if (experiment['test-score'] >= max_score):
+        max_score = experiment['test-score']
+        max_score_method = experiment
+
+print(max_score_method)
